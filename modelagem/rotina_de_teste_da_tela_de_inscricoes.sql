ebd0fa2a8f7fa0a7419b090a11c3851dc5b7002a
sgomes <sgomes@reconcavo.org.br>
2026-01-03 14:30:53 -0300

    ajustes na rotina de testes da tela de inscricoes

--- modelagem/rotina_de_teste_da_tela_de_inscricoes.sql
+++ modelagem/rotina_de_teste_da_tela_de_inscricoes.sql
@@ -1,23 +1,35 @@
-SELECT ID_Usuario FROM TB_Inscricao
-WHERE ID_Registro = (
-	SELECT ID_Registro
-	FROM TB_REGISTRO
-	WHERE
-		TP_Registro = 'Evento'
-		AND DH_Inicio > CURRENT_TIMESTAMP
-	ORDER BY DH_Inicio ASC
-	LIMIT 1
-);
+-- EXECUTE A QUERY PASSO A PASSO E SUBSTITUA OS VALORES CORRETAMENTE
 
--- 1. Verifica Eventos que o usuário está Inscrito
-SELECT mi.* FROM public.vm_minhasinscricoes mi
-JOIN TB_USUARIO u ON mi.ID_Usuario = u.ID_Usuario
-WHERE u.CD_CPF = '000000000008'
+-- 1. Verifica Evento que ainda não ocorreu e busca um usuário que está Inscrito
+REFRESH MATERIALIZED VIEW VM_MinhasInscricoes;
+SELECT 
+    mi.ID_Inscricao,
+    mi.ID_Usuario,
+    mi.Nome_Evento,
+    mi.Data_Inicio,
+    mi.Status_Atual
+FROM public.VM_MinhasInscricoes mi
+WHERE 
+    -- Filtra para pegar apenas inscrições do PRÓXIMO evento futuro
+    mi.ID_Evento = (
+        SELECT ID_Registro
+        FROM TB_REGISTRO
+        WHERE TP_Registro = 'Evento' 
+          AND DH_Inicio > CURRENT_TIMESTAMP
+        ORDER BY DH_Inicio ASC
+        LIMIT 1
+    )
+    -- Garante que seja uma inscrição Paga (ideal para testar cancelamento/estorno)
+    AND mi.Status_Atual = 'Pago'
+LIMIT 1;
 
 -- 2. Solicita cancelamento de um evento específico
 CALL public.sp_realizarcancelamentoseguro(
-	'1', -- id inscrição
-	'1', -- id usuário
+	'21', -- id inscrição
+	'9', -- id usuário
 	''
-)
+);
 
+-- 3. Consulta LOG
+SELECT * FROM TB_AuditoriaCancelamento
+WHERE ID_Inscricao = 21; -- id inscrição
